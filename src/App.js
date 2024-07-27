2a6f0eff29666d030cebf412b51f72c128a3d855
rodpicke <robbe.depickere@ugent.be>
2024-07-28 01:13:49 +0200

    add fact of the day

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DAG ELKE EN TIPSY BOELEKE</h1>
+        <p>Wij zijn de beste vrienden</p>
+        <p>en wij houden van elkaar</p>
+        <p>en wij zijn altijd samen</p>
+        <p>en wij zijn altijd blij</p>
+        <p>en wij zijn altijd samen</p>
+        <p>en wij zijn altijd blij</p>
+        <p>en wij zijn altijd samen</p>
+        <p>en wij zijn altijd blij</p>
+        <p>en wij zijn altijd samen</p>
+        <p>en wij zijn altijd blij</p>
+        <p>en wij zijn altijd samen</p>
+        <p>en wij zijn altijd blij</p>
+        <h1>weetje van de dag</h1>
+        <h2>e pluribus unum </h2>
+        <p>vertaling: uit velen één...</p>
+        <p>Deze spreuk was onofficieel het nationaal motto van de Verenigde Staten, maar wordt ook buiten de Verenigde Staten gebruikt. </p>
       </header>
     </div>
   );
