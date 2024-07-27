1588a70cfc7cefb3b612ce17bb02a1888dd72418
rodpicke <robbe.depickere@ugent.be>
2024-07-28 00:47:37 +0200

    add tipsy boeleke

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/dag elke/i);
+  const linkElement = screen.getByText(/dag elke en tipsy boeleke/i);
   expect(linkElement).toBeInTheDocument();
 });
