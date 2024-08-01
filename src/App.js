f7012ec0374a9434fbe8d972a12f51ed96f8fe9f
elkdhaen <elke.dhaenens1@gmail.com>
2024-08-01 11:50:39 +0200

    add contributor section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Contributors from './pages/Contributors';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contributors" element={<Contributors />} />
       </Routes>
     </BrowserRouter>
   );
